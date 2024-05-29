d05c17dba54218055d5defd617e0275a07f181fd
ItsMagick <magick930@googlemail.com>
2024-05-29 15:46:37 +0200

    - fuzzing types

--- run_bachelor.sh
+++ run_bachelor.sh
@@ -1,8 +1,10 @@
-find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o \
+find . \
+          \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o \
           -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o \
           -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) \
           -exec rm -R {} +
-docker run --rm -t \
+docker run \
+          --rm -t \
           --user="$(id -u):$(id -g)" \
           --net=none \
           -v "$(pwd):/tmp" \
@@ -10,15 +12,17 @@ docker run --rm -t \
           latexmk \
             -outdir=/tmp \
             -pdf /tmp/Bachelorarbeit.tex
-docker run --rm -t -\
-          -user="$(id -u):$(id -g)" \
+docker run \
+          --rm -t \
+          --user="$(id -u):$(id -g)" \
           --net=none \
           -v "$(pwd):/tmp" \
           leplusorg/latex:1.0 \
           latexmk \
             -outdir=/tmp \
             -pdf /tmp/Bachelorarbeit.tex
-find . \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o \
+find . \
+          \( -iname '*.out' -o -iname '*.toc' -o  -iname '*.log' -o -iname '*.aux' -o -iname '*.dvi' -o -iname '*.lol' -o \
           -iname '*.lof' -o -iname '*.run.xml' -o -iname '*.fdb_latexmk' -o -iname '*.fls' -o -iname '*.bbl' -o \
           -iname '*.bcf' -o -iname '*.blg' -o -iname '*.lot' \) \
           -exec rm -R {} +
